35a6c8982a439271cc08db24ee79b47c7ecce756
ipoogleduck <ipoogleduck@gmail.com>
2022-01-04 01:11:32 -0800

    Progress on apple watch
    Shared user defaults not working

--- OSU ID Watch WatchKit Extension/InterfaceController.swift
+++ OSU ID Watch WatchKit Extension/InterfaceController.swift
@@ -11,8 +11,17 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var mainImage: WKInterfaceImage!
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        if let text = UserDefaults.getString(key: .name) {
+            name.setText(text)
+        }
+        if let data = UserDefaults.getData(key: .barcodeImage), let image = UIImage(data: data) {
+            mainImage.setImage(image)
+        }
     }
     
     override func willActivate() {
